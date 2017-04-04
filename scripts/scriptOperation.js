7cbc0de56fbeda83ef78ac9bd090a83613f13788
karavai <karavai96-2010@mail.ru>
2017-04-04 14:55:24 +0300

    create function for test

--- scripts/scriptOperation.js
+++ scripts/scriptOperation.js
@@ -3,59 +3,64 @@ function clickCheckFormula() {
     var output = document.getElementById("output");
     var outputTypeFormula = document.getElementById("outputTypeFormula");
       if (verificationFormula(inputFormula)) {
+        document.getElementById("hiddenForm").hidden = false;
           output.innerHTML = "Количество подформул: " + searchSubformuls(inputFormula);
           var object = getTableTruth(inputFormula);
           var tbody = document.getElementById('tbody');
           tbody.innerHTML = objectToTable(object.table, object.sizeSymbolInFormula);
           outputTypeFormula.innerHTML = getAnswerTypeFormula();
       } else {
+         document.getElementById("hiddenForm").hidden = true;
           output.innerHTML = "Данное выражение не является формулой";
       }
 }
-
-function newTestSubformula() {
-    var test = document.getElementById("output");
-    test.innerHTML = generateFormula();
-}
-
+// Функции для тестов
 function newTestTypeFormula() {
- var test = document.getElementById("outputTypeFormula");
- test.innerHTML = generateFormula();
- var object = getTableTruth(test.textContent);
- var tbody = document.getElementById('tbody');
- tbody.innerHTML = objectToTable(object.table, object.sizeSymbolInFormula);
+ var test = document.getElementById("outputTypeFormula").value;
+ var outputCheckTypeFormula = document.getElementById("outputCheckTypeFormula");
+ if (verificationFormula(test)) {
+    outputCheckTypeFormula.innerHTML = "Данное выражение является формулой";
+    document.getElementById("hiddenForm").hidden = false;
+    var object = getTableTruth(test);
+    var tbody = document.getElementById('tbody');
+    tbody.innerHTML = objectToTable(object.table, object.sizeSymbolInFormula);   
+      } else {
+              document.getElementById("hiddenForm").hidden = true;
+          outputCheckTypeFormula.innerHTML = "Данное выражение не является формулой";
+      }
 }
 
 function clickCheckTest() {
   var formula = document.getElementById("formula").value;
-  var test = document.getElementById("output");
-  var compSearch = searchSubformuls(test.textContent);
+  var enterFormula = document.getElementById("EnterFormula").value;
   var outputResult = document.getElementById("outputResult");
-    if (formula == compSearch) {
-      outputResult.innerHTML = "Верно";
+ if (verificationFormula(enterFormula)) {
+    var compSearch = searchSubformuls(enterFormula);
+        if (formula == compSearch) {
+        outputResult.innerHTML = "Верно";
+            } else {
+            outputResult.innerHTML = "Неверно";
+            }
     } else {
-      outputResult.innerHTML = "Неверно";
+          outputResult.innerHTML = "Данное выражение не является формулой";
     }
-    newTestSubformula();
   }
 
 function clickYes() {
  var outputCheckTypeFormula = document.getElementById("outputCheckTypeFormula");
 
-    if (getN() == getCount()) {
+    if (getCount() == getN()) {
         outputCheckTypeFormula.innerHTML = "Верно, эта формула общезначимая";
     } else {
         outputCheckTypeFormula.innerHTML = "Неверно, эта формула не общезначимая";
     }
-    newTestTypeFormula();
 }
 
 function clickNo() {
  var outputCheckTypeFormula = document.getElementById("outputCheckTypeFormula");
-    if (getN() == getCount()) {
+    if (getCount() == getN()) {
         outputCheckTypeFormula.innerHTML = "Неверно, эта формула общезначимая";
     } else {
         outputCheckTypeFormula.innerHTML = "Верно, эта формула не общезначимая";
     }
-    newTestTypeFormula();
 }
